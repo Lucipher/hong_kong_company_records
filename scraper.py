54f1ac4cb535b6902296b5a78d00046ed7f336f8
Guy Freeman <git@gfrm.in>
2014-07-15 17:46:30 +0800

    unicode again

--- scraper.py
+++ scraper.py
@@ -4,7 +4,7 @@ import scraperwiki
 for crno in range(1, 10):
     crnostr = "%07d" % crno
     baseurl = "https://www.mobile-cr.gov.hk/mob/cps_criteria.do?queryCRNO="
-    html = scraperwiki.scrape(baseurl + crnostr)
+    html = scraperwiki.scrape(baseurl + crnostr).decode('utf-8')
     print html
 
     import lxml.html           
@@ -21,9 +21,9 @@ for crno in range(1, 10):
         for namesno in range(len(namestds)):
             names["Name" + str(namesno)] = namestds[namesno].text_content()
         data = {
-        'cr' : tds[1].text_content(),
-        'English Company Name' : tds[2].text_content().rsplit('\r')[1].lstrip('\n\t'),
-        'Chinese Company Name' : tds[2].text_content().rpartition('\r')[2].lstrip('\r\n\t'),
+        'cr' : tds[1].text_content().encode('utf-8'),
+        'English Company Name' : tds[2].text_content().encode('utf-8').rsplit('\r')[1].lstrip('\n\t'),
+        'Chinese Company Name' : tds[2].text_content().encode('utf-8').rpartition('\r')[2].lstrip('\r\n\t'),
         'Company Type' : tds[4].text_content()[:-1],
         'Date of incorporation' : tds[6].text_content(),
         'Company status' : tds[8].text_content()[:-1],
