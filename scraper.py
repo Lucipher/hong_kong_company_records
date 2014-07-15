9d5e649a2891815ed87155c91b9edd22e236e39f
Guy Freeman <git@gfrm.in>
2014-07-15 17:36:50 +0800

    test

--- scraper.py
+++ scraper.py
@@ -5,9 +5,10 @@ for crno in range(1, 10):
     crnostr = "%07d" % crno
     baseurl = "https://www.mobile-cr.gov.hk/mob/cps_criteria.do?queryCRNO="
     html = scraperwiki.scrape(baseurl + crnostr)
+    print html
 
     import lxml.html           
-    root = lxml.html.fromstring(html)
+    root = lxml.html.fromstring(html) # , encoding="utf-8")
 
     tds = root.cssselect("tr td tr td")
     namestds = root.cssselect("td.data")   
@@ -15,14 +16,14 @@ for crno in range(1, 10):
     if tds == []:
         pass
     else:
-        print tds[2].encode('utf-8').text_content()
+        print tds[2].text_content()
         names = {}
         for namesno in range(len(namestds)):
             names["Name" + str(namesno)] = namestds[namesno].text_content()
         data = {
         'cr' : tds[1].text_content(),
         'English Company Name' : tds[2].text_content().rsplit('\r')[1].lstrip('\n\t'),
-        'Chinese Company Name' : tds[2].encode('utf-8').text_content().rpartition('\r')[2].lstrip('\r\n\t'),
+        'Chinese Company Name' : tds[2].text_content().rpartition('\r')[2].lstrip('\r\n\t'),
         'Company Type' : tds[4].text_content()[:-1],
         'Date of incorporation' : tds[6].text_content(),
         'Company status' : tds[8].text_content()[:-1],
