3af97eace07290af020d24438a5d1e373d1d0ba7
Guy Freeman <git@gfrm.in>
2014-07-15 17:53:29 +0800

    lots of unicode

--- scraper.py
+++ scraper.py
@@ -18,20 +18,20 @@ for crno in range(1, 10):
         print tds[2].text_content().encode('utf-8')
         names = {}
         for namesno in range(len(namestds)):
-            names["Name" + str(namesno)] = namestds[namesno].text_content()
+            names["Name" + str(namesno)] = namestds[namesno].text_content().encode('utf-8')
         data = {
         'cr' : tds[1].text_content().encode('utf-8'),
         'English Company Name' : tds[2].text_content().encode('utf-8').rsplit('\r')[1].lstrip('\n\t'),
         'Chinese Company Name' : tds[2].text_content().encode('utf-8').rpartition('\r')[2].lstrip('\r\n\t'),
-        'Company Type' : tds[4].text_content()[:-1],
-        'Date of incorporation' : tds[6].text_content(),
-        'Company status' : tds[8].text_content()[:-1],
-        'Active status' : tds[10].text_content()[:-1],
-        'Remarks' : tds[11].text_content()[16:],
-        'Winding up mode' : tds[13].text_content()[:-1],
-        'Date of Dissolution' : tds[15].text_content(),
-        'Register of Charges' : tds[17].text_content()[:-1],
-        'Important Note' : tds[18].text_content()[15:].lstrip('\r\n\t'),
+        'Company Type' : tds[4].text_content().encode('utf-8')[:-1],
+        'Date of incorporation' : tds[6].text_content().encode('utf-8'),
+        'Company status' : tds[8].text_content().encode('utf-8')[:-1],
+        'Active status' : tds[10].text_content().encode('utf-8')[:-1],
+        'Remarks' : tds[11].text_content().encode('utf-8')[16:],
+        'Winding up mode' : tds[13].text_content().encode('utf-8')[:-1],
+        'Date of Dissolution' : tds[15].text_content().encode('utf-8'),
+        'Register of Charges' : tds[17].text_content().encode('utf-8')[:-1],
+        'Important Note' : tds[18].text_content().encode('utf-8')[15:].lstrip('\r\n\t'),
         'Name History' : names
         }
         
