cb6d1f267254253141e6cac40692702df5d5663b
Guy Freeman <git@gfrm.in>
2014-07-16 09:39:13 +0800

    no encoding

--- scraper.py
+++ scraper.py
@@ -17,27 +17,26 @@ for crno in range(1, 10):
         pass
     else:
         for idx, val in enumerate(tds):
-            print idx, ":", val.text_content().encode('utf-8')
-        print tds[2].text_content().encode('utf-8')
+            print idx, ":", val.text_content()
+        print tds[2].text_content()
         names = {}
         for namesno in range(len(namestds)):
-            names["Name" + str(namesno)] = namestds[namesno].text_content().encode('utf-8')
+            names["Name" + str(namesno)] = namestds[namesno].text_content()
         data = {
-        'cr' : tds[1].text_content().encode('utf-8'),
-        'English Company Name' : tds[2].text_content().encode('utf-8').rsplit('\r')[1].lstrip('\n\t'),
-        'Chinese Company Name' : tds[2].text_content().encode('utf-8').rpartition('\r')[2].lstrip('\r\n\t'),
-        'Company Type' : tds[4].text_content().encode('utf-8')[:-1],
-        'Date of incorporation' : tds[6].text_content().encode('utf-8'),
-        # 'Company status' : tds[8].text_content().encode('utf-8')[:-1],
-        'Active status' : tds[8].text_content().encode('utf-8')[:-1],
-        'Remarks' : tds[9].text_content().encode('utf-8')[16:],
-        'Winding up mode' : tds[11].text_content().encode('utf-8')[:-1],
-        'Date of Dissolution' : tds[13].text_content().encode('utf-8'),
-        'Register of Charges' : tds[15].text_content().encode('utf-8')[:-1],
-        'Important Note' : tds[16].text_content().encode('utf-8')[16:].lstrip('\r\n\t') # ,
+        'cr' : tds[1].text_content(),
+        'English Company Name' : tds[2].text_content().rsplit('\r')[1].lstrip('\n\t'),
+        'Chinese Company Name' : tds[2].text_content().rpartition('\r')[2].lstrip('\r\n\t'),
+        'Company Type' : tds[4].text_content()[:-1],
+        'Date of incorporation' : tds[6].text_content(),
+        # 'Company status' : tds[8].text_content()[:-1],
+        'Active status' : tds[8].text_content()[:-1],
+        'Remarks' : tds[9].text_content()[16:],
+        'Winding up mode' : tds[11].text_content()[:-1],
+        'Date of Dissolution' : tds[13].text_content(),
+        'Register of Charges' : tds[15].text_content()[:-1],
+        'Important Note' : tds[16].text_content()[16:].lstrip('\r\n\t') # ,
         # 'Name History' : names
         }
         
 
     scraperwiki.sqlite.save(unique_keys=['cr'], data=data)
-        
