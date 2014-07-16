055672f4878db72d22b94623ec7d4a9459a4bea9
Guy Freeman <git@gfrm.in>
2014-07-16 09:35:46 +0800

    no to_s

--- scraper.py
+++ scraper.py
@@ -21,20 +21,20 @@ for crno in range(1, 10):
         print tds[2].text_content().encode('utf-8')
         names = {}
         for namesno in range(len(namestds)):
-            names["Name" + str(namesno)] = namestds[namesno].text_content().encode('utf-8').to_s
+            names["Name" + str(namesno)] = namestds[namesno].text_content().encode('utf-8')
         data = {
         'cr' : tds[1].text_content().encode('utf-8'),
-        'English Company Name' : tds[2].text_content().encode('utf-8').rsplit('\r')[1].lstrip('\n\t').to_s,
-        'Chinese Company Name' : tds[2].text_content().encode('utf-8').rpartition('\r')[2].lstrip('\r\n\t').to_s,
-        'Company Type' : tds[4].text_content().encode('utf-8')[:-1].to_s,
-        'Date of incorporation' : tds[6].text_content().encode('utf-8').to_s,
+        'English Company Name' : tds[2].text_content().encode('utf-8').rsplit('\r')[1].lstrip('\n\t'),
+        'Chinese Company Name' : tds[2].text_content().encode('utf-8').rpartition('\r')[2].lstrip('\r\n\t'),
+        'Company Type' : tds[4].text_content().encode('utf-8')[:-1],
+        'Date of incorporation' : tds[6].text_content().encode('utf-8'),
         # 'Company status' : tds[8].text_content().encode('utf-8')[:-1],
-        'Active status' : tds[8].text_content().encode('utf-8')[:-1].to_s,
-        'Remarks' : tds[9].text_content().encode('utf-8')[16:].to_s,
-        'Winding up mode' : tds[11].text_content().encode('utf-8')[:-1].to_s,
-        'Date of Dissolution' : tds[13].text_content().encode('utf-8').to_s,
-        'Register of Charges' : tds[15].text_content().encode('utf-8')[:-1].to_s,
-        'Important Note' : tds[16].text_content().encode('utf-8')[16:].lstrip('\r\n\t').to_s # ,
+        'Active status' : tds[8].text_content().encode('utf-8')[:-1],
+        'Remarks' : tds[9].text_content().encode('utf-8')[16:],
+        'Winding up mode' : tds[11].text_content().encode('utf-8')[:-1],
+        'Date of Dissolution' : tds[13].text_content().encode('utf-8'),
+        'Register of Charges' : tds[15].text_content().encode('utf-8')[:-1],
+        'Important Note' : tds[16].text_content().encode('utf-8')[16:].lstrip('\r\n\t') # ,
         # 'Name History' : names
         }
         
