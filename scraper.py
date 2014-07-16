affe87d9cf48ebc322b19aa53c2acb30da510e27
Guy Freeman <git@gfrm.in>
2014-07-16 12:40:07 +0800

    Improve Important Note scraping

--- scraper.py
+++ scraper.py
@@ -36,7 +36,7 @@ for crno in range(1, 10):
         'Winding up mode' : tds[11].text_content()[:-1],
         'Date of Dissolution' : tds[13].text_content(),
         'Register of Charges' : tds[15].text_content()[:-1],
-        'Important Note' : tds[16].text_content()[16:].lstrip('\r\n\t')
+        'Important Note' : tds[16].text_content().replace(u"重要事項：","").lstrip('\r\n\t')
         }
         data.update(names)
 
