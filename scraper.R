42526d49fc94c65b990aad5dd89c0b2fdf57c0f2
Guy Freeman <startup@gfrm.in>
2015-01-03 19:02:21 +0800

    Parallel

--- scraper.R
+++ scraper.R
@@ -1,4 +1,5 @@
-library(data.table)
+library(parallel)
+                                        #library(data.table)
 library(rvest)
 library(lubridate)
 library(stringr)
@@ -23,7 +24,7 @@ extractnames <- function(names) {
         return(list(deleteslashes(namesenglish), deleteslashes(nameschinese)))
 }
 
-scrape <- function(crno, crdata) {
+scrape <- function(crno) {
     crnostr = sprintf("%07d", crno)
     baseurl = "https://www.mobile-cr.gov.hk/mob/cps_criteria.do?queryCRNO="
     url = paste0(baseurl, crnostr)
@@ -33,7 +34,7 @@ scrape <- function(crno, crdata) {
 
     if (length(html_nodes(html, "img")) > 0) {
         print(c("sleeping, then trying again", crnostr))
-        save(scrapelist, file = "scrapelist.Rdata")
+#        save(scrapelist, file = "scrapelistlist.Rdata")
         Sys.sleep(50)
         html <- html(url, encoding = "utf-8")
     }
@@ -41,49 +42,44 @@ scrape <- function(crno, crdata) {
     if (!is.null(html_node(html, "body"))) {
         if (length(grep('沒有紀錄與輸入的查詢資料相符', html_text(html_node(html, "body"))) > 0)) {
             print('NO MATCHING RECORD FOUND FOR THE SEARCH INFORMATION INPUT!')
-            return(crdata)
+            return(list(crno = crno))
         }
     }
 
     tds <- html_text(html_nodes(html, "td:nth-child(2)"))
 
-    crdata <- rbindlist(list(crdata, list(crno = tds[1], companytype = deleteslashes(tds[2]), incorporationdate = dmy(tds[3]), status = deleteslashes(tds[4]), windingupmode = deleteslashes(tds[5]), dissolutiondate = dmy(tds[6]), registeravailable = deleteslashes(tds[7]))), fill = TRUE)
-    setkey(crdata, crno)
+    crdata <- list(crno = tds[1], companytype = deleteslashes(tds[2]), incorporationdate = dmy(tds[3]), status = deleteslashes(tds[4]), windingupmode = deleteslashes(tds[5]), dissolutiondate = dmy(tds[6]), registeravailable = deleteslashes(tds[7]))
     
     companyname <- str_replace(deleteslashes(html_text(html_node(html, "td tr:nth-child(2) td"))), "公司名稱：", "")
     companynameextract <- extractnames(companyname)
-    crdata[crnostr,currentnameenglish := companynameextract[[1]]]
-    crdata[crnostr,currentnamechinese := companynameextract[[2]]]
+    crdata[["currentnameenglish"]] = companynameextract[[1]]
+    crdata[["currentnamechinese"]] = companynameextract[[2]]
 
-    crdata[crnostr, remarks := deleteslashes(html_text(html_node(html, ".sameasbody")))]
-    crdata[crnostr, note := str_replace(html_text(html_node(html, "tr:nth-child(10) td")), fixed("重要事項：\r\n\t\t\t\t\t"), "")]
+    crdata[["remarks"]] = deleteslashes(html_text(html_node(html, ".sameasbody")))
+    crdata[["note"]] = str_replace(html_text(html_node(html, "tr:nth-child(10) td")), fixed("重要事項：\r\n\t\t\t\t\t"), "")
     
     names <- html_nodes(html, ".data")
     for (name in 1:length(names)) {
         tempname <- html_text(names[name])
-        crdata[crnostr, paste0("namesdate", name) := dmy(str_sub(tempname, 1, 10))]
+        crdata[[paste0("namesdate", name)]] = dmy(str_sub(tempname, 1, 10))
         tempname <- str_sub(tempname, 11)
         tempextract <- extractnames(tempname)
-        crdata[crnostr, paste0("namesenglish", name) := tempextract[[1]]]
-        crdata[crnostr, paste0("nameschinese", name) := tempextract[[2]]]
+        crdata[[paste0("namesenglish", name)]] = tempextract[[1]]
+        crdata[[paste0("nameschinese", name)]] = tempextract[[2]]
     }
     
     return(crdata)
 }
 
-if (file.exists("scrapelist.Rdata")) {
-    load("scrapelist.Rdata")
-} else {
-    scrapelist <- data.table(crno = "0", companytype = "blah")
-    setkey(scrapelist, crno)
-}
+#if (file.exists("scrapelist.Rdata")) {
+#    load("scrapelist.Rdata")
+#} else {
+#    scrapelist <- data.table(crno = "0", companytype = "blah")
+#    setkey(scrapelist, crno)
+#}
 
-maxcr = 2120960
-mincr = scrapelist[,max(as.numeric(crno))]
+maxcr = 50 # 2120960
+mincr = 1 # scrapelist[,max(as.numeric(crno))]
 
-for (crnonumber in (mincr:maxcr)) {
-    if (is.na(scrapelist[sprintf("%07d", crnonumber), companytype])) {
-        scrapelist <- scrape(crnonumber, scrapelist)
-    }
-    save(scrapelist, file = "scrapelist.Rdata")    
-}
+scrapelist <- mclapply(mincr:maxcr, scrape)
+
